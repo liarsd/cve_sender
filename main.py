d762c6418fac7ea3c5fbf42c93ea131b7d22d973
liarsd <d.stepanov@tut.by>
2023-11-16 17:01:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import json
 def get_nvd_feed():
     url = 'https://nvd.nist.gov/feeds/json/cve/1.1/nvdcve-1.1-recent.json.zip' # NVD Feed URL
     wget.download(url)
-    command = 'unzip -o nvdcve-1.0-recent.json.zip' # Unzip json.gz file
+    command = 'unzip -o nvdcve-1.1-recent.json.zip' # Unzip json.gz file
     os.system(command)
 
 def get_cpes():
