156764448e3288a8323be9474d82917adbb29909
liarsd <d.stepanov@tut.by>
2023-11-17 13:03:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def parse_nvd_feed(cpes):
     return message,cve_count
 
 def slack_block_format(product, description, id):
-    block = ',{"type": "section", "text": {"type": "mrkdwn","text": "/color *Product:* ff0000 ' + product + '\n *CVE ID:* ' + id + '\n *Description:* ' + description + '\n "}}, {"type": "divider"}'
+    block = ',{"type": "section", "text": {"type": "mrkdwn","text": "*Product:* ' + product + '\n *CVE ID:* ' + id + '\n *Description:* ' + description + '\n "}}, {"type": "divider"}'
     return block
  
 def send_slack_alert(message,cve_count):
